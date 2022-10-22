e8e47ce27ad57c0f116751c06d02275599cc3717
gracexkan <grace.kan38@gmail.com>
2022-10-23 10:45:36 +1100

    fixed header

--- client/src/layout/Header.jsx
+++ client/src/layout/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
 const Header = () => {
-  return <header className="header">Team Pink</header>;
+  return <header className="header"><div>Team Pink</div></header>;
 };
 export default Header;

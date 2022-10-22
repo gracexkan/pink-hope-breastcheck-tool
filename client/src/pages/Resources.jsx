c484119f8bf4fee893deb3f5f89c5a310f09a832
Justin Tan <justintws10@gmail.com>
2022-10-22 13:30:01 +1100

    working on resources

--- client/src/pages/Resources.jsx
+++ client/src/pages/Resources.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+
 
 const Resources = () => {
   return (
-    <p>This is the resource page</p>
+    <div>
+      <h2>Resources</h2>
+
+      <a href="https://www.pinkhope.org.au/blog/how-to-check-your-breasts-and-know-your-normal">
+        Self Check Instructions
+      </a>
+
+      <Link to="/resources/faqs">FAQs</Link>
+
+      <Link>Symptoms</Link>
+
+      <Link>Find Professionals</Link>
+
+      <Link>Blog / Stories</Link>
+
+    </div>
   );
 }
 

4320a2eb9676fd48ec7bb5d31544d3cd6f0cc978
Justin Tan <justintws10@gmail.com>
2022-10-22 22:36:36 +1100

    styled calendar

--- client/src/layout/Footer.jsx
+++ client/src/layout/Footer.jsx
@@ -23,22 +23,22 @@ const Footer = () => {
         >
 					<Link color="text.secondary" href="/" underline="none" variant="body2">
 						<IconButton aria-label="Calendar">
-							<FaCalendarDay />
+							<FaCalendarDay color="#73313b"/>
 						</IconButton>
 					</Link>
 					<Link color="text.secondary" href="self-check" underline="none" variant="body2">
 						<IconButton aria-label="Self Check">
-							<FaHeartbeat />
+							<FaHeartbeat color="#73313b"/>
 						</IconButton>
 					</Link>
 					<Link color="text.secondary" href="resources" underline="none" variant="body2">
 						<IconButton aria-label="Resources">
-							<FaBook />
+							<FaBook	color="#73313b" />
 						</IconButton>
 					</Link>
 					<Link color="text.secondary" href="profile" underline="none" variant="body2">
 						<IconButton aria-label="Profile">
-							<FaUserCircle />
+							<FaUserCircle color="#73313b"/>
 						</IconButton>
 					</Link>
         </Box>

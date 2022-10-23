aab3cff0333be904f9f70ac0fbc5038b880c0b79
Sally Sun <sallyysun101@gmail.com>
2022-10-23 13:06:10 +1100

    Breastcheck (#10)
    * alot of stuff
    
    * mergine main (#3)
    
    * Added model
    
    * decision tree model cleaned
    
    Co-authored-by: Ray <raiyan.ahmed@gmail.com>
    
    * hi
    
    * moved calform
    
    * did some css
    
    * added q
    
    * finish ifnish
    
    Co-authored-by: Ray <raiyan.ahmed@gmail.com>

--- client/src/components/breastcheck/BCFinish.jsx
+++ client/src/components/breastcheck/BCFinish.jsx
@@ -1,24 +1,53 @@
 import React, { useState } from "react";
+<<<<<<< HEAD
+import { Popover } from "antd";
+import Button from "@mui/material/Button";
+import BCResult from "./BCResult";
+=======
 import Button from "@mui/material/Button";
 import BCSuccess from "./BCSuccess";
+>>>>>>> 01e1b7952f12acab4756ae985ca3470e13a5d7f7
 
 function BCFinish() {
   const buttonStyle = {
     backgroundColor: "#73313b",
     color: "#fff",
   };
+<<<<<<< HEAD
+  const [open, setOpen] = useState(false);
+  const hide = () => {
+    setOpen(false);
+  };
+  const handleOpenChange = (newOpen) => {
+    setOpen(newOpen);
+=======
   const [isSuccess, setIsSuccess] = useState(false);
   const showSuccess = (e) => {
     setIsSuccess((show) => !show);
+>>>>>>> 01e1b7952f12acab4756ae985ca3470e13a5d7f7
   };
 
   return (
     <div>
+<<<<<<< HEAD
+      <Popover
+        content={<BCResult onClick={hide} />}
+        // title="Title"
+        trigger="click"
+        open={open}
+        onOpenChange={handleOpenChange}
+      >
+        <Button variant="contained" style={buttonStyle}>
+          Finish Self Check
+        </Button>
+      </Popover>
+=======
       <Button variant="contained" style={buttonStyle} onClick={showSuccess}>
         {"Finish"}
         show BCSuccess{" "}
       </Button>
       <BCSuccess onClose={showSuccess} show={isSuccess} />
+>>>>>>> 01e1b7952f12acab4756ae985ca3470e13a5d7f7
     </div>
   );
 }
